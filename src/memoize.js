bd6c00b6b6714bca8e70e8cee39cd496f2dad287
Sergei Lovtsov <lovtsov_sergei@mail.ru>
2018-08-22 11:32:54 +0300

    Implement memoize function

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,20 @@
-export function memoize() {
-    // TODO
+export function memoize(func) {
+    if (typeof (func) !== 'function' || !arguments.length) return null;
+    const cache = new Map();
+    return function memoized(...args) {
+        let key = '';
+        if (!args.length) key = 'null';
+        else {
+            for (let i = 0; i < args.length; i += 1) {
+                key += args[i] + typeof args[i];
+            }
+        }
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        const result = func.apply(this, args);
+        cache.set(key, result);
+        return result;
+    };
 }
+
